60e901b8483d57c81d2d5c25b4880e3a9aa2bbb9
cyZ52 <1349706931@qq.com>
2023-12-18 10:26:26 +0800

    f

---
+++ client/next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+require('events').EventEmitter.defaultMaxListeners = 0
+
+
+const nextConfig = {
+  output: 'standalone',
+  reactStrictMode: false,
+  compress: true,
+
+  webpack(config) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true
+    };
+    config.module.rules = config.module.rules.concat([
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack']
+      }
+    ]);
+
+    return config;
+  }
+};
+
+module.exports = nextConfig;
